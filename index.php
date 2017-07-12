a64a478e9ac077985e25d23ca8445cf1966bb3eb
Jaques Moura <jaquesmoura@gmail.com>
2017-07-12 00:06:16 -0400

    Refatoração com uso de metodo e atributo static

--- index.php
+++ index.php
@@ -15,18 +15,7 @@ else
 	$ord = 1;
 } 
 
-
-CODE\Cliente\ListaDeClientes::$client_list = array ( '1' => new CODE\Cliente\ClientePF(1,"Jaques Moura","385.222.682-15","Av. Parintins 489 - Cachoeirinha",5),
-                                '2' => new CODE\Cliente\ClientePF(2,"Lucilana Moura","222.222.222-22","Av. Parintins 489 - Cachoeirinha",5),
-                                '3' => new CODE\Cliente\ClientePF(3,"Evelyn Moura","333.222.222-22","Av. Parintins 489 - Cachoeirinha",5),
-                                '4' => new CODE\Cliente\ClientePF(4,"Emanuele Moura","444.222.222-22","Av. Parintins 489 - Cachoeirinha",5),
-                                '5' => new CODE\Cliente\ClientePF(5,"Kaleb Santos","555.222.222-22","Av. Dois 543 - Centro",0),
-                                '6' => new CODE\Cliente\ClientePJ(6,"Fred Krueger Company","93.362.629/0001-62","Av. Dois 543 - Centro",5),
-                                '7' => new CODE\Cliente\ClientePF(7,"Jason Mraz","777.222.222-22","Av. Itacolomy 200 - Cidade Nova",0),
-                                '8' => new CODE\Cliente\ClientePJ(8,"Jackson Five Company","18.672.681/0001-08","Av. Wakusese 111 - Flores",5),
-                                '9' => new CODE\Cliente\ClientePF(9,"Luana Piovani","999.222.222-22","Av. Wandyn 2389 - Flores",0),
-                                '10' => new CODE\Cliente\ClientePF(10,"Maysa Carvalho","111.222.222-22","Av. Conte Teles 999 - Aleixo",0)
-                            ); 
+CODE\Cliente\ListaDeClientes::init();
 
 if ($ord == '1'){
 	asort(CODE\Cliente\ListaDeClientes::$client_list);
--- src/CODE/Cliente/ListaDeClientes.php
+++ src/CODE/Cliente/ListaDeClientes.php
@@ -2,9 +2,27 @@
 
 namespace CODE\Cliente;
 
+use CODE\Cliente\ClientePF;
+use CODE\Cliente\ClientePJ;
+
 class ListaDeClientes{
 
     public static $client_list;  
+
+    public static function init()
+    {
+    	self::$client_list = array ( '1' => new ClientePF(1,"Jaques Moura","385.222.682-15","Av. Parintins 489 - Cachoeirinha",5),
+                                '2' => new ClientePF(2,"Lucilana Moura","222.222.222-22","Av. Parintins 489 - Cachoeirinha",5),
+                                '3' => new ClientePF(3,"Evelyn Moura","333.222.222-22","Av. Parintins 489 - Cachoeirinha",5),
+                                '4' => new ClientePF(4,"Emanuele Moura","444.222.222-22","Av. Parintins 489 - Cachoeirinha",5),
+                                '5' => new ClientePF(5,"Kaleb Santos","555.222.222-22","Av. Dois 543 - Centro",0),
+                                '6' => new ClientePJ(6,"Fred Krueger Company","93.362.629/0001-62","Av. Dois 543 - Centro",5),
+                                '7' => new ClientePF(7,"Jason Mraz","777.222.222-22","Av. Itacolomy 200 - Cidade Nova",0),
+                                '8' => new ClientePJ(8,"Jackson Five Company","18.672.681/0001-08","Av. Wakusese 111 - Flores",5),
+                                '9' => new ClientePF(9,"Luana Piovani","999.222.222-22","Av. Wandyn 2389 - Flores",0),
+                                '10' => new ClientePF(10,"Maysa Carvalho","111.222.222-22","Av. Conte Teles 999 - Aleixo",0)
+                            );
+    }
 
 }
 
